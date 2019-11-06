95b679d09b4080ef18d77c766e69416e5dce35c4
Scott Hoisington <hoyzington@gmail.com>
2019-11-06 20:56:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,12 +5,24 @@ require 'directors_database'
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 
+
+
 def directors_totals(nds)
   result = {}
-  nil
+  
+  
+  
 end
+
+
+
+
+
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  
+  
+  
 end
