1329d4729304b5467aa1f243ecff8c25edfa6e3a
Scott Hoisington <hoyzington@gmail.com>
2019-11-06 23:33:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,7 +17,7 @@ def directors_totals(nds)
   result = {}
   which_dir = 0
   while which_dir < nds.length do
-    result[nds[which_dir][:name]] = gross_for_director
+    result[nds[which_dir][:name]] = gross_for_director(nds, which_dir)
     which_dir += 1
   end
   result
