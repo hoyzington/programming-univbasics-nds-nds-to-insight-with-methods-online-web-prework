c45e6f98c71f328f4e7266919b2bd03be9a37223
Scott Hoisington <hoyzington@gmail.com>
2019-11-06 23:49:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,7 +20,7 @@ end
 def gross_for_director(director_data)
   earnings = 0
   which_mov = 0
-  while which_mov < nds[which_dir][:movies].length do
+  while which_mov < director_data[:movies].length do
     earnings += nds[which_dir][:movies][which_mov][:worldwide_gross]
     which_mov += 1
   end
