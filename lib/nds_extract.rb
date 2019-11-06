3dc04dfbbeebda0c5367df89696c3d3400c3d285
Scott Hoisington <hoyzington@gmail.com>
2019-11-06 20:58:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,21 @@ require 'directors_database'
 def directors_totals(nds)
   result = {}
   
-  
+
+  which_dir = 0
+  while dirs < nds.count do
+    earnings = 0
+    movs = 0
+    while movs < nds[dirs][:movies].count
+      earnings += nds[dirs][:movies][movs][:worldwide_gross]
+      movs += 1
+    end
+    hash[nds[dirs][:name]] = earnings
+    dirs += 1
+  end
+  hash
+end
+
   
 end
 
